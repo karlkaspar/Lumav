49aef00c213d6e659f94658ae851293e9e6dd6e1
karlkaspar <karlkasparkulesha@gmail.com>
2022-01-12 19:24:36 +0200

    Added header & footer components, basic scss -> css system

---
+++ my-app/src/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Footer = () => {
+  return <footer>I am footer</footer>
+};
+
+export default Footer;
---
+++ my-app/src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Header = () => {
+    return <header>
+        <strong>Lumav</strong>
+        <div>
+            Products in cart: 
+        </div>
+    </header>;
+};
+
+export default Header
